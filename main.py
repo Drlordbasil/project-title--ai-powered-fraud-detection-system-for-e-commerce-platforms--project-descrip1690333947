0607628125840e30a800b803c181da0ee8107a05
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 21:09:49 -0400

    AI-enhanced and auto-formatted code

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-import numpy as np
-import pandas as pd
-from sklearn.ensemble import IsolationForest
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, classification_report
-import matplotlib.pyplot as plt
 import seaborn as sns
-There seems to be a problem in the code you provided. It is missing the necessary imports. Please add the following imports:
-
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import IsolationForest
+import pandas as pd
+import numpy as np
 ```python
 ```
 
-Once you add these imports, your code should work fine.
+The necessary imports have been added to the code.
